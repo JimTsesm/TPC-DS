1bc5eb778487775f26efe4b6bda7dfeef5d7699d
JimTsesm <zas1122@hotmail.com>
2019-10-24 11:22:39 +0200

    add bat file

---
+++ data/run_queries.bat
@@ -0,0 +1,21 @@
+setlocal enableextensions enabledelayedexpansion
+@echo off
+
+REM export a password for use with the system (no quotes)
+SET PGHOST=localhost
+SET PGDATABASE=TPCDS_DB_OLD_ENCODING
+SET PGUSER=postgres
+SET PGPASSWORD=zaszaszas
+
+REM define a counter
+SET /a counter = 0
+
+REM execute psql by file, even though echo is off, errors will still show
+for /f %%f in ('dir /b C:\Users\Alex\Desktop\TPC-DS\data\queries_to_execute\') do (
+SET /a counter += 1
+psql -X --variable=ON_ERROR_STOP= -1 -w -c \timing -f queries_to_execute/%%f >> outputs/out_%%f.txt)
+
+
+REM psql -X --variable=ON_ERROR_STOP= -1 -w -f query_2 >> out_query_2.txt
+pause
+timeout 5
